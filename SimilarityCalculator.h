bc7e11c12eee805e8b8d459796b50bb10697c460
지웅 <seth42@naver.com>
2026-05-18 22:43:33 +0900

    알고리즘, 사용자 평점 비교 및 단순 유사도 계산

---
+++ SimilarityCalculator.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include "Rating.h"
+
+class SimilarityCalculator {
+public:
+    static int calculate(const std::vector<Rating>& user1, const std::vector<Rating>& user2);
+};
